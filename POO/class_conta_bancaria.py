0b20321f34c2fd0b83a92262c1de6eeb10070a75
Senac Tatuape <senac@localhost.localdomain>
2024-01-22 12:05:49 -0300

    arquivos da aula 22/01/2024

---
+++ POO/class_conta_bancaria.py
@@ -0,0 +1,7 @@
+class Contabancaria:
+    def _init_(self):
+        self._nbanco=0
+        self._nagencia=0
+        self._nconta=""
+        self._titular=""
+        self._saldo=0.0
---
+++ POO/obj_conta_corrente.py
@@ -0,0 +1,6 @@
+from class_conta_corrente import ContaCorrente as cc
+
+conta.abrirconta()
+conta = cc()
+conta.abrirconta(35,12,"123548-10","Marcia",3500,1000)
+
